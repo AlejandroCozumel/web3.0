ca212023f4a85d02b4470b3275bd29b542e089ad
Alejandro Zamora <alejandroo14@gmail.com>
2022-04-09 01:16:38 -0500

    Realizado el header y hero

---
+++ client/src/views/LandingPage.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Welcome from "../components/Welcome";
+import Footer from "../components/Footer";
+import Services from "../components/Services";
+import Transactions from "../components/Transactions";
+
+const LandingPage = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
